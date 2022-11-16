29d2f0a25113bc007d203f949ba06c7fd5a552f1
Natalia Martínez <80662024+natymartinez04@users.noreply.github.com>
2022-11-16 00:51:52 -0500

    Fix: divide and conquer function wasn't finding the correct pair of coordinates with the smallest distance

--- ClosestPair_NataliaMartinez/src/closestpair_nataliamartinez/Brute.java
+++ ClosestPair_NataliaMartinez/src/closestpair_nataliamartinez/Brute.java
@@ -14,19 +14,16 @@ import java.util.ArrayList;
  * @author natymartinez04
  */
 public class Brute {
-
-    Coordinate coordinate1 = new Coordinate(null,0,0);
-    Coordinate coordinate2 = new Coordinate(null,0,0);
+    
+    
+    ArrayList<Pairs> pairs = new ArrayList();
     long comparaciones = 0;
-    
-    public Coordinate getCoordinate1() {
-        return coordinate1;
-    }
 
-    public Coordinate getCoordinate2() {
-        return coordinate2;
+    public ArrayList<Pairs> getPairs() {
+        return pairs;
     }
     
+  
     public long getComparasiones() {
         return comparaciones;
     }
@@ -36,7 +33,7 @@ public class Brute {
     }
     
     
-    
+    //Brute force function: Finds the minimun distance between
     public double bruteForce(ArrayList<Coordinate> coordinates,double dmin){ 
         int n = coordinates.size();
         for (int i = 0; i<n;i++){
@@ -44,8 +41,8 @@ public class Brute {
                if (i!=j){
                   if (distance(coordinates.get(i), coordinates.get(j))<=dmin){
                     dmin = distance(coordinates.get(i), coordinates.get(j));
-                    coordinate1 = coordinates.get(i);
-                    coordinate2 = coordinates.get(j);
+                    pairs.add(new Pairs(coordinates.get(i),coordinates.get(j),dmin));
+                    
                   }
                } 
                comparaciones++;
@@ -54,28 +51,25 @@ public class Brute {
         
         return dmin;
     }
-   
     
+   
+    //Divide and Conquer function: Recursively finds the minimun distance between two points
     public double closestRecursive(ArrayList<Coordinate> coordinates,double dmin){
         int n = coordinates.size();
         comparaciones++;
         if (n<=3){
-            dmin = bruteForce(coordinates,dmin);
+            return bruteForce(coordinates,dmin);
         }else{
             int mid = n/2;
             Coordinate Cmid = (Coordinate) coordinates.get(mid);
             double dl = closestRecursive(subArray(coordinates,0,mid),dmin);
-            double dr = closestRecursive(subArray(coordinates,mid,n),dmin);     
+            double dr = closestRecursive(subArray(coordinates,mid,n),dmin);    
             dmin = Math.min(dl,dr);
             ArrayList<Coordinate> strip = new ArrayList<>();
             coordinates = Strip(strip,coordinates,Cmid,0,dmin);
         }
-        if (Math.min(dmin, bruteForce(coordinates,dmin))==dmin){
-            return dmin;
-        }else{
-           return bruteForce(coordinates,dmin); 
-        }
         
+        return bruteForce(coordinates,dmin); 
     }
 
 
@@ -102,9 +96,31 @@ public class Brute {
         return Math.sqrt(Math.pow(i.getX()-j.getX(),2)+Math.pow(i.getY()-j.getY(),2));
     }
 
-
-
-    
-
 }
 
+class Pairs {
+    
+    Coordinate coordinate1;
+    Coordinate coordinate2;
+    double minDistance;
+
+    public Pairs(Coordinate coordinate1, Coordinate coordinate2, double minDistance) {
+        this.coordinate1 = coordinate1;
+        this.coordinate2 = coordinate2;
+        this.minDistance = minDistance;
+    }
+
+    public Coordinate getCoordinate1() {
+        return coordinate1;
+    }
+
+    public Coordinate getCoordinate2() {
+        return coordinate2;
+    }
+
+    public double getMinDistance() {
+        return minDistance;
+    }
+
+    
+}
